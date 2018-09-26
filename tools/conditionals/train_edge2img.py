658e5c5c96cfe6777fe7f2f6bcb9e0841a8bf97f
Hi-king <hikingko1@gmail.com>
2018-09-27 01:12:30 +0900

    :art: glob pattern as args

--- tools/conditionals/train_edge2img.py
+++ tools/conditionals/train_edge2img.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 import sys
+import pipe
+import glob
 
 thisfilepath = pathlib.Path(__file__)
 sys.path.append(str(thisfilepath.parent.parent.parent))
@@ -10,17 +12,17 @@ import train_conditional
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset_directory', type=pathlib.Path)
+    parser.add_argument('dataset_glob')
     parser.add_argument('--edge', action=argparse._StoreTrueAction)
     train_conditional.shared_args(parser)
     args = parser.parse_args()
     args.prefix = "edge2img" + ("_edge" if args.edge else "")
+
+    paths = glob.glob(args.dataset_glob) | pipe.select(pathlib.Path) | pipe.as_list
     if args.edge:
         dataset = chainer_progressive_gan.datasets.Edge2ImgDataset(
-            list(args.dataset_directory.glob("*.jpg")),
-            resize=(args.resize, args.resize))
+            paths, resize=(args.resize, args.resize))
     else:
         dataset = chainer_progressive_gan.datasets.Sketch2ImgDataset(
-            list(args.dataset_directory.glob("*.jpg")),
-            resize=(args.resize, args.resize))
+            paths, resize=(args.resize, args.resize))
     train_conditional.main(args, dataset)
